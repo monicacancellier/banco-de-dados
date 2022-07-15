50c60d5bbf823088bd03f4d7c5da76989d88e082
monicacancellier <104033056+monicacancellier@users.noreply.github.com>
2022-07-15 17:03:15 -0300

    att

---
+++ sql.sql
@@ -0,0 +1,11 @@
+create database heitor;																									
+																									
+use heitor;																									
+																									
+create table usuarios(																									
+id_usuario int AUTO_INCREMENT PRIMARY key,																									
+nome varchar(30),																									
+telefone varchar(40),																									
+email varchar(15),																									
+senha varchar(32)																									
+);		
